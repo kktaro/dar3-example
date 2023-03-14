a847bea1e8b40e4d0bf4779ba794fe84ee0dc674
Takuya Mutai <takuya.mutai@glpgs.com>
2023-03-14 10:26:31 +0900

    リポジトリを入れ替えできるようにする

--- lib/feature/todo/repository/todo_repository.dart
+++ lib/feature/todo/repository/todo_repository.dart
@@ -4,22 +4,35 @@ import 'package:dart3_sample/feature/todo/repository/extension/domain_todo_exten
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final todoRepositoryProvider = Provider<TodoRepository>((ref) {
-  return TodoRepository(ref.watch(todosDatabaseProvider));
+  return TodoRepositoryImpl(ref.watch(todosDatabaseProvider));
 });
 
-final class TodoRepository {
-  TodoRepository(this._database);
+abstract class TodoRepository {
+  Future<int> add(domain.Todo todo);
+  Future<int> update(domain.Todo todo);
+  Future<void> delete(int id);
+  Stream<List<domain.Todo>> watchAll();
+  Future<domain.Todo> fetch(int id);
+}
+
+final class TodoRepositoryImpl implements TodoRepository {
+  TodoRepositoryImpl(this._database);
 
   final TodosDatabase _database;
 
+  @override
   Future<int> add(domain.Todo todo) => _database.upsert(todo.toDTO());
 
+  @override
   Future<int> update(domain.Todo todo) => _database.upsert(todo.toUniqueDTO());
 
+  @override
   Future<void> delete(int id) => _database.deleteSingle(id);
 
+  @override
   Stream<List<domain.Todo>> watchAll() => _database.watchAll()
     .map((event) => event.map((e) => DomainTodoExtension.fromDTO(e)).toList());
 
+  @override
   Future<domain.Todo> fetch(int id) async => DomainTodoExtension.fromDTO(await _database.fetchSingle(id));
 }
